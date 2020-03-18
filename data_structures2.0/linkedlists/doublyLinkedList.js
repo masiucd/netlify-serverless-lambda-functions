6177ab4cd6b137ae68b84a5bdd68011844895785
Masiu <ciszekmarcell@gmail.com>
2020-03-18 12:58:27 +0100

    ADD

---
+++ data_structures2.0/linkedlists/doublyLinkedList.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class LinkedListXs {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  showList() {}
+
+  insertFirst(data) {}
+
+  insertEnd(data) {}
+
+  get(index) {}
+
+  insert(index, data) {}
+
+  delete(index) {}
+}
+
+const myLL = new LinkedListXs();
+
+console.log(myLL);
