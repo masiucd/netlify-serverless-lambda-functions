9d228216de4a2f900358bba0260d1bfaf9ca7cb1
Masiu <ciszekmarcell@gmail.com>
2020-03-22 12:42:51 +0100

    qs

---
+++ data_structures2.0/stacks_and_queues/qs.ts
@@ -0,0 +1,16 @@
+// Queues with stacks
+
+class Qs {
+  first: string[];
+
+  last: string[];
+
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+}
+
+const qs = new Qs();
+
+console.log(qs);
