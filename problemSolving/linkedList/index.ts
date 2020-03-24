d663bec3d0dadbd2d4b4dc421f9d412326a0da9a
Masiu <ciszekmarcell@gmail.com>
2020-03-24 14:09:36 +0100

    linked lists

--- problemSolving/linkedList/index.ts
+++ problemSolving/linkedList/index.ts
@@ -64,10 +64,74 @@ class LinkedList {
       size: this.size,
     };
   }
+
+  getNode(index: number): INode {
+    let counter = 0;
+    let node = this.head;
+    while (index !== counter) {
+      counter += 1;
+      node = node.next;
+    }
+    return node;
+  }
+
+  insert(index: number, data: number) {
+    if (index === 0) return this.insertFirst(data);
+    if (index < 0 || index > this.size) return undefined;
+    const newNode = new Node(data);
+    const prevNode = this.getNode(index - 1);
+    const pointer = prevNode.next;
+    prevNode.next = newNode;
+    newNode.next = pointer;
+    this.size += 1;
+    return this;
+  }
+
+  removeHead() {
+    if (!this.head.next) return undefined;
+    this.head = this.head.next;
+    this.size -= 1;
+    return this;
+  }
+
+  remove(index: number) {
+    if (index === 0) return this.removeHead();
+    if (index < 0 || index > this.size) return undefined;
+    const prevNode = this.getNode(index - 1);
+    const nodeToDelete = prevNode.next;
+    prevNode.next = nodeToDelete.next;
+    this.size -= 1;
+    return this;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  reverse() {
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this;
+  }
 }
 
 const ll = new LinkedList();
 ll.insertFirst(8);
+ll.insertEnd(321);
 ll.insertFirst(12);
 ll.insertEnd(22);
+ll.insert(3, 100);
+ll.insert(0, 200);
+ll.removeHead();
+ll.remove(3);
+ll.reverse();
 console.log(ll.showList());
