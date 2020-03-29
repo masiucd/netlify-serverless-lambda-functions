693e6d15b4c807fff597d266518fd5fa19ca49aa
Masiu <ciszekmarcell@gmail.com>
2020-03-29 16:39:51 +0200

    lookup

--- data_structures2.0/trees/tree.js
+++ data_structures2.0/trees/tree.js
@@ -1,8 +1,8 @@
 class Node {
-  constructor(value) {
+  constructor(data) {
     this.left = null;
     this.right = null;
-    this.value = value;
+    this.data = data;
   }
 }
 
@@ -11,12 +11,46 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(value) {
-    // Code here
+  insert(data) {
+    const newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (data < currentNode.data) {
+          // go left
+          if (!currentNode.left) {
+            // if there is nothing there
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // go right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            // stop looping
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
   }
 
-  lookup(value) {
-    // Code here
+  lookup(data) {
+    if (!this.root) return false;
+
+    let currentNode = this.root;
+    while (currentNode) {
+      if (data < currentNode.data) {
+        currentNode = currentNode.left;
+      } else if (data > currentNode.data) {
+        currentNode = currentNode.right;
+      } else if (currentNode.data === data) return currentNode;
+    }
+    return false;
   }
   // remove
 }
@@ -29,14 +63,16 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-JSON.stringify(traverse(tree.root));
+
+// console.log(JSON.stringify(traverse(tree.root)));
+// console.log(tree.lookup(231231));
 
 //     9
 //  4     20
 // 1  6  15  170
 
 function traverse(node) {
-  const tree = { value: node.value };
+  const tree = { data: node.data };
   tree.left = node.left === null ? null : traverse(node.left);
   tree.right = node.right === null ? null : traverse(node.right);
   return tree;
