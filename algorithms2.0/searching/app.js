57c7419e55050d2482c7ea3de333ed994f1a6987
Masiu <ciszekmarcell@gmail.com>
2020-04-11 11:41:54 +0200

    bs

---
+++ algorithms2.0/searching/app.js
@@ -0,0 +1,5 @@
+const things = ['apa', 'banana', 'godzilla', 'songo-jho', 'naruto'];
+
+const findBanana = things.indexOf('banana');
+const finNaruto = things.findIndex(item => item === 'naruto');
+console.log(finNaruto);
