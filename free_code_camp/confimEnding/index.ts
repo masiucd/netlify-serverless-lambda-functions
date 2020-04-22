72b9e8593891c44646e4e84581af580690f54fcc
Masiu <ciszekmarcell@gmail.com>
2020-04-22 11:54:16 +0200

    ADD

---
+++ free_code_camp/confimEnding/index.ts
@@ -0,0 +1,9 @@
+function confirmEnding(str: string, target: string) {
+  const targetLen = target.length;
+  return str.slice(-targetLen) === target;
+  // return str.substr(str.length - 1) === target;
+}
+
+const x = confirmEnding('Bastian', 'n'); // true
+const xs = confirmEnding('Congratulation', 'on'); // true
+console.log(xs);
