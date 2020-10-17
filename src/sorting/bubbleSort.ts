fba7c61a83ff07d6d22931c568ff9a5befac8810
Masiu <ciszekmarcell@gmail.com>
2020-10-17 12:21:37 +0200

    more sorting

--- src/sorting/bubbleSort.ts
+++ src/sorting/bubbleSort.ts
@@ -1,3 +1,19 @@
-const bubbleSort = (xs: number[]): number[] => {
-  //
+const swap = (list: number[], idx1: number, idx2: number) =>
+  [list[idx1], list[idx2]][(list[idx2], list[idx1])]
+
+export const bubbleSort = (xs: number[]): number[] => {
+  for (let i = 0; i < xs.length; i++) {
+    for (let j = 0; j < xs.length; j++) {
+      if (xs[j] > xs[j + 1]) {
+        let temp = xs[j]
+        xs[j] = xs[j + 1]
+        xs[j + 1] = temp
+      }
+    }
+  }
+  return xs
 }
+
+const list = [19, 100, 2, 45, 2, 3, 99, 1, 5]
+
+const res = bubbleSort(list)
