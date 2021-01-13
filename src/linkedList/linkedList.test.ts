e13bb42e4ebe6314c7d0cafd8d21e76fcbb6532e
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-01-13 17:05:02 +0100

    (tech): clean up repo, new starter for rx-js

--- src/linkedList/linkedList.test.ts
+++
@@ -1,6 +0,0 @@
-import * as ll from "./index"
-
-describe("LinkedList", () => {
-  test("size should be incremented and decremented depending on adding a new node ore removing a node from the linkedlist", () => {})
-  test("adding a new node to the list correctly", () => {})
-})
