cd2a9d7622c6aeeb38026e7b4b643bc79e26d0e3
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-03-11 08:47:52 +0100

    feat load images from serverless funcs and using unsplash data

--- functions/load-corgis.js
+++ functions/load-corgis.js
@@ -4,11 +4,31 @@ exports.handler = async () => {
   const corgis = await fetch("https://no-cors-api.netlify.app/api/corgis");
   const corgisResp = await corgis.json();
 
+  const unsplashPromise = await fetch("https://api.unsplash.com/collections/48405776/photos", {
+    headers: {
+      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+    },
+  }).then((response) => response.json());
+
+  // We want them to run in parallel to get them at the same time
+  const [unsplashData] = await Promise.all([unsplashPromise]);
+
+  const completeData = corgisResp.map((corgi) => {
+    const photo = unsplashData.find((p) => corgi.id === p.id);
+
+    return {
+      ...corgi,
+      alt: photo.alt_description,
+      credit: photo.user.name,
+      url: `${photo.urls.raw}&auto=format&fit=crop&w=300&h=300&q=80&crop=entropy`,
+    };
+  });
+
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(corgisResp),
+    body: JSON.stringify(completeData),
   };
 };
--- src/components/corgis.tsx
+++ src/components/corgis.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { corgis } from "../data/corgis";
+import { Corgi } from "../utils/types";
 import { CorgiItem } from "./corgi-item";
 
 const grid = css`
@@ -11,16 +12,21 @@ const grid = css`
   justify-items: center;
 `;
 
-const loadCorgis = async () => {
-  const corgis = await fetch("/.netlify/functions/load-corgis");
-  const response = await corgis.json();
-  return response;
-};
-
 export const Corgis = () => {
+  const [corgisData, setCorgisData] = useState<Corgi[]>([]);
+
+  useEffect(() => {
+    const loadCorgis = async () => {
+      const corgis = await fetch("/.netlify/functions/load-corgis");
+      const response = await corgis.json();
+      setCorgisData(response);
+    };
+    loadCorgis();
+  }, []);
+
   return (
     <section className={grid}>
-      {corgis.map((corgi) => (
+      {corgisData.map((corgi) => (
         <CorgiItem key={corgi.id} corgi={corgi} />
       ))}
     </section>
