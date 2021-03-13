24abbdc4e4bc594881d2d698244296c450fe0fd9
masiucd <ciszekmarcell@gmail.com>
2021-03-13 13:35:39 +0100

    feat: serverlessf unction

---
+++ functions/util/hasura.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+
+exports.hasuraRequest = async function ({ query, variables }) {
+  const result = await fetch(process.env.HASURA_URL, {
+    metod: "POST",
+    headers: {
+      "X-Hasura-Admin-Secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+    },
+    body: JSON.stringify({ query, variables }),
+  }).then((res) => res.json());
+
+  if (!result || !result.data) {
+    console.error(result);
+    return [];
+  }
+
+  return result.data;
+};
--- src/components/corgi-item.tsx
+++ src/components/corgi-item.tsx
@@ -1,5 +1,5 @@
 import { css, cx } from "@emotion/css";
-import React from "react";
+import React, { useState } from "react";
 import { Corgi } from "../utils/types";
 
 interface CorgiItemProps {
@@ -7,60 +7,84 @@ interface CorgiItemProps {
 }
 
 const corgiStyles = css`
-  display: flex;
-  flex-flow: column wrap;
-  box-shadow: var(--shadowLg);
-  border-radius: var(--border-radius);
-  flex: 1;
+  & {
+    display: flex;
+    flex-flow: column wrap;
+    box-shadow: var(--shadowLg);
+    border-radius: var(--border-radius);
+    flex: 1;
+  }
 `;
 
 const corgiHeaderStyles = css`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  & {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
-  img {
-    border-radius: var(--border-radius);
-  }
-  small {
-    display: block;
-    padding: 0.45rem 1rem;
+    img {
+      border-radius: var(--border-radius);
+    }
+    small {
+      display: block;
+      padding: 0.45rem 1rem;
+    }
   }
 `;
 
 const bodyStyles = css`
-  display: flex;
-  flex-flow: column wrap;
-  flex: 1;
-  padding: 1rem;
-  box-shadow: var(--shadowInner);
-  background-color: var(--primary);
-  border-radius: var(--border-radius);
-  p {
-    margin-bottom: 1.5rem;
-    &:nth-of-type(1) {
-      font-size: 1.4rem;
-      border-bottom: 1px solid var(--white);
-    }
-  }
-  button {
-    margin-top: auto;
-    cursor: pointer;
-    padding: 0.5rem;
-    font-size: 1.1em;
+  & {
+    display: flex;
+    flex-flow: column wrap;
+    flex: 1;
+    padding: 1rem;
+    box-shadow: var(--shadowInner);
+    background-color: var(--primary);
     border-radius: var(--border-radius);
-    border: 2px solid var(--textColor);
-    background-color: var(--white);
-    transition: var(--main-trans);
-    &:hover {
-      background-color: var(--danger);
+    p {
+      margin-bottom: 1.5rem;
+      &:nth-of-type(1) {
+        font-size: 1.4rem;
+        border-bottom: 1px solid var(--white);
+      }
+    }
+    button {
+      margin-top: auto;
+      cursor: pointer;
+      padding: 0.5rem;
+      font-size: 1.1em;
+      border-radius: var(--border-radius);
+      border: 2px solid var(--textColor);
+      background-color: var(--white);
+      transition: var(--main-trans);
+      &:hover {
+        background-color: var(--danger);
+      }
     }
   }
 `;
 
 export const CorgiItem: React.FC<CorgiItemProps> = ({ corgi }) => {
+  const [boobCount, setBoobCount] = useState(0);
+  const [isBooping, setIsBooping] = useState(false);
+
+  const handleBoop = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    setIsBooping(true);
+    fetch("/.netlify/functions/add-boop", {
+      method: "POST",
+      body: JSON.stringify({ id: corgi.id }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setBoobCount(data.updated.count);
+      });
+    setBoobCount((p) => p + 1);
+    setIsBooping(false);
+  };
+
   return (
     <li className={corgiStyles}>
       <div className={cx(corgiHeaderStyles, "header-img")}>
@@ -70,7 +94,9 @@ export const CorgiItem: React.FC<CorgiItemProps> = ({ corgi }) => {
       <div className={bodyStyles}>
         <p>{corgi.name}</p>
         <p>favorite-song: {corgi.favoriteSong}</p>
-        <button>Booped {corgi.boops} times</button>
+        <button onClick={(e) => handleBoop(e)} data-id={corgi.id} disabled={isBooping}>
+          {isBooping ? "hold on booping" : "boop " + boobCount + " times"}
+        </button>
       </div>
     </li>
   );
